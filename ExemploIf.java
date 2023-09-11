34cb41b8497a2399025e05961e0db49a57c2c242
Raí joia <megac154@gmail.com>
2023-09-11 20:47:13 -0300

    ap03(selecao): import e estrutura

---
+++ ExemploIf.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class ExemploIf {
+    static public void main(String [] args) {
+        // declaração de varáveis
+        
+
+        // entrada
+        
+
+        // processamento
+
+
+        // saida
+
+        
+    }
+}
