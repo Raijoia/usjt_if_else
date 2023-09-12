1087783dd5af8744246b2bdf2e1636f2728a2370
Raí joia <megac154@gmail.com>
2023-09-11 21:07:34 -0300

    ap03(selecao): if implementado

--- ExemploIf.java
+++ ExemploIf.java
@@ -3,16 +3,13 @@ import javax.swing.JOptionPane;
 public class ExemploIf {
     static public void main(String [] args) {
         // declaração de varáveis
-        
-
         // entrada
+        double nota = Double.parseDouble(JOptionPane.showInputDialog("Digite a nota"));
         
-
         // processamento
-
-
         // saida
-
-        
+        if(nota >= 70) {
+            JOptionPane.showMessageDialog(null, "Aprovado");
+        } 
     }
 }
